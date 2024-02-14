41e43436500aa03c5b347907591645c6a81c4d25
sillo <58334334+root1234567891234@users.noreply.github.com>
2024-02-14 21:45:22 +0900

    Create code.cs

---
+++ code.cs
@@ -0,0 +1,13 @@
+static string split_sum(string[] content,int num)
+    {
+        int i = 0;
+        string output = "";
+        foreach (string line in content) {
+            if (a >= num)
+            {
+                output += line + " ";
+                }
+            i++;
+            }    
+        return output;
+    }
